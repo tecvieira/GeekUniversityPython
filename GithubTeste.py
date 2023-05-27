5bba7534ab92017e413f02bb0eeb31e43dd77f3f
Robson Vieira <tecvander.vieira@gmail.com>
2023-05-27 17:23:34 -0300

    atualizou com linha no windows

--- GithubTeste.py
+++ GithubTeste.py
@@ -1,5 +1,6 @@
 """
 Utilizando Github para atualizar repositório remoto
 Agora editei em outra maquina para testar github 270523-> 06:08hs
+Esta linha foi criada no windows para teste github 270523
 """
 print(' Olá Mundo em 27/05/23')
