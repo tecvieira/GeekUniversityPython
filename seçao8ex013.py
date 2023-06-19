27e512c46922aa2ab00c6085043a87f2a5bd5376
Robson Vieira <tecvander.vieira@gmail.com>
2023-06-19 16:21:57 -0300

    atualização 16:21 em 19/06/23

--- seçao8ex013.py
+++ seçao8ex013.py
@@ -1,16 +1,16 @@
-def operacoes_matematica(a, b):
-    print("\n"
-          "    ESCOLHA A OPERAÇÃO"
-          "    (+) adição\n"
-          "    (-) subtração\n"
-          "    (*) multiplicação\n"
-          "    (/) divisão\n"
-          "     ")
-    resp = int(input('Selecione a operação: '))
-    # falta acabar esta função
+def escolhe_sinal(a, b):
+    sinal = ''
+    sinal = input('Sinal da operação ("+", "-", "*", "/" ), escolha sua opção: ')
+    if sinal == '+':
+        print(a + b)
+    elif sinal == '-':
+        print(a - b)
+    elif sinal == '*':
+        print(a * b)
+    elif sinal == '/':
+        print(a / b)
+    else:
+        print('Valor inválido, tente outra vez')
 
 
-
-
-
- operacoes_matematica(a=int(input('Informe 1º número:')), b=int(input('Informe 2º número')))
+escolhe_sinal(a=int(input('Valor de "a": ')), b=int(input('Valor de "b": ')))
