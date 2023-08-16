625042ac1ad7bc27d3cf4cb1d7a89d10bedbcb86
Robson Vieira <87854460+tecvieira@users.noreply.github.com>
2023-08-16 20:09:27 -0300

    atualizado 20 09

---
+++ FazendoCompras/soma_compras.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+def mensagem1():
+    print('-=' * 15)
+    print('       FAZENDO COMPRAS.')
+    print('-=' * 15)
+
+
+def cabecalho():
+    data_atual = datetime.date.today()
+    hora_atual = datetime.date.now()
+
+
+
+
+print(mensagem1())
+print(cabecalho())
