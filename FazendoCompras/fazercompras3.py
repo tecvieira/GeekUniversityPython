9c8eb6340d40e7abd33913b2543639f7347ff991
Robson Vieira <87854460+tecvieira@users.noreply.github.com>
2023-08-24 20:07:15 -0300

    atualização 20:00 em 24 08 23

--- FazendoCompras/fazercompras3.py
+++ FazendoCompras/fazercompras3.py
@@ -2,24 +2,25 @@ from datetime import datetime
 from time import sleep
 
 
-def cabecalho():
+def cabecalho(msg):
     print('-=' * 15)
     print('       FAZENDO COMPRAS')
     print('-=' * 15)
-    print(local)
+    print(msg)
     data_hora = datetime.now()
-    data_hora_txt = data_hora.strftime('%d/%m/%y - %H:%M')
+    data_hora_txt = data_hora.strftime('%d/%m/%y - %H:%M\n')
+    with open('novacompra3.txt', 'a') as arquivo:
+        arquivo.write(f'{msg}\n')
+        arquivo.write(data_hora_txt)
     print(data_hora_txt)
 
 
 def imprimir_txt(msg):
-    with open('novacompra2.txt', 'a') as arquivo:
+    with open('novacompra3.txt', 'a') as arquivo:
         arquivo.write(f'{msg}\n')
 
 
-local = str(input('Informe nome do Mercado: ')).upper()
-mostra_cabecalho = cabecalho()
-print(mostra_cabecalho)
+print(cabecalho(input('Nome do Mercado:').upper()))
 pergunta = ''
 soma_produto = 0
 pedido = []
@@ -44,16 +45,15 @@ while True:
     if pergunta == 'N':
         break
 print('Finalizando...')
-sleep(3)
+sleep(2)
 print('-=' * 15)
 print('Produto : Qtde : Vl.Unit : Vl.Total')
-imprimir_txt(f'{local}\n')
-imprimir_txt(f'{datetime.now()}\n')
-imprimir_txt('Produto-Qtde-Vunit-Total\n')
+imprimir_txt('-=' * 15)
+imprimir_txt('Produto/ Qtde/ VUnit/ Total')
 for p in pedido:
     imprimir_txt(p)
     print(p)
 print('-=' * 15)
 imprimir_txt('-=' * 15)
-imprimir_txt(f'Valor total a pagar: R${soma_produto:.2f}')
-print(f'\033[32mVALOR TOTAL A PAGAR = R${soma_produto:.2f}\033[m')
+imprimir_txt(f'TOTAL A PAGAR: R$ {soma_produto:.2f}')
+print(f'\033[32mVALOR TOTAL A PAGAR = R$ {soma_produto:.2f}\033[m')
