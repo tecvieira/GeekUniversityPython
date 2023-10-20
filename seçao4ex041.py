85684aaf9436aa4adc1fc39fd5b984b04c44eaa7
Robson Vieira <tecvander.vieira@gmail.com>
2023-10-19 21:13:06 -0300

    atualizando 191023

---
+++ seçao4ex041.py
@@ -0,0 +1,10 @@
+"""from time import sleep
+vl_hora = float(input('Informe o valor da hora trabalhada: R$ '))
+qtd_hora = float(input('Quantidade de horas trabalhadas: '))
+print('Calculando o valor a receber...')
+sleep(1)
+valor = (qtd_hora * vl_hora)
+print(f'Horas trabalhadas.... {qtd_hora}'
+      f'Valor da hora .......{vl_hora:.2f}'
+      f'Total a receber R${valor:.2f}')
+"""
