c3f88474b402df17a97c12b503845af5d21ffbf8
Robson Vieira <tecvander.vieira@gmail.com>
2023-10-19 22:15:08 -0300

    corigindo sec4ex41

--- seçao4ex041.py
+++ seçao4ex041.py
@@ -8,3 +8,15 @@ print(f'Horas trabalhadas.... {qtd_hora}'
       f'Valor da hora .......{vl_hora:.2f}'
       f'Total a receber R${valor:.2f}')
 """
+
+
+def valor_hora(hora, valor):
+    """Calcula o valor da hora trabalhada"""
+    calculo = hora * valor
+    aumento = (calculo * 10) / 100
+    return print(f'O valor da hora trabalhada com 10% de acrescimo R$ {calculo + aumento:.2f}')
+
+
+hor = float(input('Quantas horas trabalhadas: '))
+val = float(input('Qual o valor da hora: '))
+valor_hora(hor, val)
