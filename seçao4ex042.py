de5e59369190b58af34f272b3d478ec04860603f
Robson Vieira <tecvander.vieira@gmail.com>
2024-01-26 22:10:36 -0300

    260124_22:10

--- seçao4ex042.py
+++ seçao4ex042.py
@@ -1,19 +1,11 @@
-def gratificacao(sal):
-    reajuste = (sal * 5) / 100
-    return print(f'Gratificação sobre o salário base R$ {reajuste:.2f}')
+def reajusta_salario():
+    nome = str(input('Informe o seu nome: '))
+    salario = float(input('Informe seu salário bruto: '))
+    novo_salario = ((salario * 5) / 100 + salario)
+    imposto = (novo_salario * 7) / 100
+    salario_liquido = novo_salario - imposto
+    print(f'O funcionário, {nome} pagou de imposto R$ {imposto:.2f} seu salário líquido é: R${salario_liquido:.2f} ')
 
 
-def imposto(sal):
-    desconto = (sal * 7) / 100
-    return print(f'Imposto sobre o salário R${desconto:.2f}')
-
-
-def saldo_final(sal):
-    return sal - desconto + requests
-
-
-salario = float(input('Informe o salário base: '))
-print(f'Salario base.......................R${salario:.2f}')
-gratificacao(salario)
-imposto(salario)
-print(f'Saldo a receber R$ {saldo_final(salario)}')
+print('==== Calculando seu Salário ====')
+reajusta_salario()
